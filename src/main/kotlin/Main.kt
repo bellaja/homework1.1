c40d16e8ab47baf9f0ef0276aacbe1d72d06b217
bellaja <bellaja185@gmail.com>
2024-03-12 17:20:07 +0300

    first commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,6 @@ package ru.netology
 fun main() {
     println("Комиссия составляет ${calculateComission(4800)} рублей.")
 }
-
 fun calculateComission(amount: Int, commissionRate: Double = 0.0075, minCommissionRate: Int = 35): Int {
     var comission = (amount * commissionRate).toInt()
     if (comission < minCommissionRate) {
